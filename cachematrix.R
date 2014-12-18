5356b00c1bb5134a3e6ea73a89c700d148182f48
rmtachi <rempel.michael@gmail.com>
2014-12-18 13:47:41 +0900

    functions for getting/setting matrix and its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## December 18, 2104
+## R programming ass 2
+## functions for creating and accessing a matrix and its inverse
 
-## Write a short comment describing this function
-
+## initializes a matrix and creates function handles to
+## get/set matrix and inverse
+## takes matrix, defaults to 1x1 NA matrix
 makeCacheMatrix <- function(x = matrix()) {
+	Inv <- NULL
+	set <- function(y) {
+		x <<- y
+		Inv <<- NULL
+	}
+	get <- function() x
+	setInv <- function(inv) Inv <<- inv
+	getInv <- function() Inv
+	list(set = set, get = get,
+		 setInv = setInv,
+		 getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
-
+## takes list object created with makeCacheMatrix and retrieves inverse
+## of matrix. Computes inverse if doesn't already exist
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ret <- x$getInv()
+	if(!is.null(ret)) {
+		message("getting cached data")
+		return(ret)
+	}
+	mat <- x$get()
+## 'solve' computes x for Ax = b, set b = Identity to compute inverse
+	ret <- solve(a=mat, b=diag(nrow(mat)), ...)
+	x$setInv(ret)
+	ret
 }
